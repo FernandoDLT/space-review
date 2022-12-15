e3b031af97f19206e64130cab6c26152aed545ac
FernandoDLT <96254499+FernandoDLT@users.noreply.github.com>
2022-12-14 20:37:37 -0600

    init

---
+++ main.js
@@ -0,0 +1,27 @@
+//  ******Arrays******
+// Create a function that takes in an array of numbers, 
+// alert the sum.
+
+let numbs = [2, 5, 8, 3, 100]
+let total = numbs.reduce( ( acc,c) => acc + c, 0 )
+// let  total = numbs.reduce( (acc, c) => acc + Number(c), 0 )
+alert(total)
+
+// let num = [5, 10, 10, 4]
+// let sum = num.reduce((acc,c) => acc + c, 0)
+// alert(sum)
+
+
+// let nums = ["5", 10, 12, 4]
+// let total = nums.reduce( (acc,c) => acc + Number(c), 0 )
+
+// alert(total)
+
+// Create a function that takes in an array of numbers, 
+// return a new array of numbers that is every original number squared
+
+// Create a funtion that takes in a string
+// Print the reverse of that string to the console
+
+// Create a funtion that takes in a string
+// Alert if the string is a palindrome or not
